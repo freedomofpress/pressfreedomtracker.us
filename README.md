7e46964155513f36033960bdc710cbccc2339b39
racheltstevens <racheltstevens@gmail.com>
2017-04-06 16:28:09 -0400

    Configures database to default to postgreSQL
    * adds psycopg2 dependency
    * updates settings file
    * updates README
    
    Close #2

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Database configuration
 
-Set the database url as `DATABASE_URL` in your `env` file.  The default is `sqlite:///db.sqlite3`.
+Set the database url as the `DATABASE_URL` enviromnent variable.
+Alternatively, use the default url, supplying `DATABASE_USER`, `DATABASE_PASSWORD` and `DATABASE_NAME`
+enviromnent variables. (Defaults to a locally run postgreSQL database).
+
--- pressfreedom/settings/base.py
+++ pressfreedom/settings/base.py
@@ -93,8 +93,13 @@ WSGI_APPLICATION = 'pressfreedom.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 import dj_database_url
 
+default_url = "postgres://{}:{}@localhost:5432/{}".format(
+    os.environ.get('DATABASE_USER'),
+    os.environ.get('DATABASE_PASSWORD'),
+    os.environ.get('DATABASE_NAME'))
+
 DATABASES = {
-    'default': dj_database_url.config(default="sqlite:///db.sqlite3")
+    'default': dj_database_url.config(default=default_url)
 }
 
 
