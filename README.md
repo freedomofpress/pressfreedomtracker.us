9d19f07b1d69c0c1323c88c5658cb01ec9c2ba3e
racheltstevens <racheltstevens@gmail.com>
2017-04-06 15:40:42 -0400

    Change settings to use dj-database-url
    * 12-factor goodness
    * add dj-database-url to requirements
    * Add instructions to README

---
+++ README.md
@@ -0,0 +1,3 @@
+## Database configuration
+
+Set the database url as `DATABASE_URL` in your `env` file.  The default is `sqlite:///db.sqlite3`.
--- pressfreedom/settings/base.py
+++ pressfreedom/settings/base.py
@@ -91,12 +91,10 @@ WSGI_APPLICATION = 'pressfreedom.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default="sqlite:///db.sqlite3")
 }
 
 
