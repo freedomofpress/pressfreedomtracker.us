217716df8d024e9bf71cc207cc160728324d15a6
racheltstevens <racheltstevens@gmail.com>
2017-04-07 12:59:16 -0400

    Add and configure django-webpack-loader

--- pressfreedom/settings/dev.py
+++ pressfreedom/settings/dev.py
@@ -2,9 +2,6 @@ from __future__ import absolute_import, unicode_literals
 
 from .base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '(g4bj*$%zf4tqdaas8#ch3-mz_27n+*-973tpxap9zmdz8ii_u'
 
@@ -16,3 +13,4 @@ try:
     from .local import *
 except ImportError:
     pass
+
