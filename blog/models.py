be10edd9c942d53aeaf62e8fc5cac7f71379d62b
racheltstevens <racheltstevens@gmail.com>
2017-04-10 11:33:07 -0400

    Create BlogPost model
    From our models doc:
    https://docs.google.com/document/d/1f4M0GRao8rrd1qN-pm_gH0-yoV5tElIBFlJcYJBiZ_I/

--- blog/models.py
+++ blog/models.py
@@ -1,22 +1,15 @@
 from django.core.validators import MaxValueValidator
 from django.db import models
 
-from wagtail.contrib.settings.models import BaseSetting
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, PageChooserPanel, StreamFieldPanel
 from wagtail.wagtailcore import blocks
 from wagtail.wagtailcore.fields import StreamField, RichTextField
 from wagtail.wagtailcore.models import Page, Orderable
 
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 
-class BlogIndexPage(Page):
-    intro = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
-    ]
-
-class BlogPage(BaseSetting):
+class BlogPage(Page):
   publication_datetime = models.DateTimeField(
         help_text='Past or future date of publication'
     )
@@ -32,9 +25,27 @@ class BlogPage(BaseSetting):
         blank=True
     )
 
+  organization = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+
+  author = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+
   content_panels = Page.content_panels + [
     FieldPanel('publication_datetime'),
-    FieldPanel('body'),
-    FieldPanel('teaser_text')
+    StreamFieldPanel('body'),
+    FieldPanel('teaser_text'),
+    PageChooserPanel('organization', 'common.OrganizationPage'),
+    PageChooserPanel('author', 'common.PersonPage'),
   ]
 
