3aa13610b1cca90b99dfb8432162fbb8f8780c6f
racheltstevens <racheltstevens@gmail.com>
2017-04-19 12:33:59 -0400

    Swap out kebab tag for django's slugify filter
    This filter already existed!

--- common/templatetags/kebab.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='kebab')
-def kebab(string):
-    """Converts a string to lowercase and kebab-case"""
-    return string.lower().replace(' ', '-')
