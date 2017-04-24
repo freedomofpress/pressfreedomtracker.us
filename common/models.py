edeca2b9fce9a329e37d5c295829f2154b69b4df
racheltstevens <racheltstevens@gmail.com>
2017-04-24 15:48:16 -0400

    Adds SimplePageWithSidebar
    Uses SideBarPage mixin from freedom.press
    Close #20

--- common/models.py
+++ common/models.py
@@ -1,14 +1,52 @@
 from django.db import models
 
 from wagtail.wagtailcore.models import Page, Orderable
-from wagtail.wagtailadmin.edit_handlers import FieldPanel,  InlinePanel, StreamFieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, StreamFieldPanel
 from wagtail.wagtailcore import blocks
 from wagtail.wagtailcore.fields import RichTextField, StreamField
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 from modelcluster.fields import ParentalKey
+from wagtail.wagtailsnippets.edit_handlers import SnippetChooserPanel
 from taggit.models import TaggedItemBase
+
+
+class BaseSidebarPageMixin(models.Model):
+    """
+    A mixin that gives a model a sidebar menu field and the ability to
+    intelligently use its own sidebar menu or a parent's sidebar menu.
+    """
+
+    sidebar_menu = models.ForeignKey(
+        'menus.Menu',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='If left empty, page will use parent\'s sidebar menu'
+    )
+
+    settings_panels = [
+        SnippetChooserPanel('sidebar_menu'),
+    ]
+
+    def get_sidebar_menu(self):
+        """
+        Return own sidebar menu if it exists. Otherwise, return the nearest
+        ancestor's sidebar menu.
+        """
+
+        if self.sidebar_menu:
+            return self.sidebar_menu
+
+        try:
+            return self.get_parent().specific.get_sidebar_menu()
+        except AttributeError:
+            return None
+
+    class Meta:
+        abstract = True
 
 
 class OrganizationIndexPage(Page):
@@ -84,6 +122,20 @@ class SimplePage(Page):
     content_panels = Page.content_panels + [
         StreamFieldPanel('body'),
     ]
+
+
+class SimplePageWithSidebar(BaseSidebarPageMixin, Page):
+    body = StreamField([
+        ('rich_text', blocks.RichTextBlock(icon='doc-full', label='Rich Text')),
+        ('image', ImageChooserBlock()),
+        ('raw_html', blocks.RawHTMLBlock()),
+    ])
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
+
+    settings_panels = Page.settings_panels + BaseSidebarPageMixin.settings_panels
 
 
 class Tag(TaggedItemBase):
