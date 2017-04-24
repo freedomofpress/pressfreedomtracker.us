65c01d5c675218a7071fb71542d9faaf2ae72e82
racheltstevens <racheltstevens@gmail.com>
2017-04-24 11:18:15 -0400

    Add incident index page field to homepage
    Will help with related incidents

--- home/models.py
+++ home/models.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import, unicode_literals
+from django.db import models
 
-from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.wagtailcore import blocks
 from wagtail.wagtailcore.fields import StreamField
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailimages.blocks import ImageChooserBlock
+from modelcluster.fields import ParentalKey
 
 
 class HomePage(Page):
@@ -14,6 +16,12 @@ class HomePage(Page):
         ('raw_html', blocks.RawHTMLBlock()),
     ], null=True)
 
+    incident_index_page = ParentalKey('incident.IncidentIndexPage', related_name='homepage',
+                                      null=True, blank=True, on_delete=models.SET_NULL)
+
+
     content_panels = Page.content_panels + [
         StreamFieldPanel('about'),
+        FieldPanel('incident_index_page')
     ]
+
