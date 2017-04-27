3b2ea0333981069f4c8a570c13cb5db12b569c2f
racheltstevens <racheltstevens@gmail.com>
2017-04-27 16:23:08 -0400

    Change Homepage 'about' to a RichTextField
    From a streamfield.
    Close #31

--- home/models.py
+++ home/models.py
@@ -1,20 +1,15 @@
 from __future__ import absolute_import, unicode_literals
 from django.db import models
 
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, StreamFieldPanel
-from wagtail.wagtailcore import blocks
-from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailcore.models import Page, Orderable
-from wagtail.wagtailimages.blocks import ImageChooserBlock
+
 from modelcluster.fields import ParentalKey
 
 
 class HomePage(Page):
-    about = StreamField([
-        ('rich_text', blocks.RichTextBlock(icon='doc-full', label='Rich Text')),
-        ('image', ImageChooserBlock()),
-        ('raw_html', blocks.RawHTMLBlock()),
-    ], null=True)
+    about = RichTextField(blank=True, null=True)
 
     about_page = models.ForeignKey(
         'wagtailcore.Page',
@@ -41,7 +36,7 @@ class HomePage(Page):
     )
 
     content_panels = Page.content_panels + [
-        StreamFieldPanel('about'),
+        FieldPanel('about'),
         FieldPanel('about_page'),
         FieldPanel('blog_index_page'),
         FieldPanel('incident_index_page'),
