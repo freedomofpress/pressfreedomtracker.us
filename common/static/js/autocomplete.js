979ce4177a02bd4b44e4ee7ba79187cced558455
Emily Horsman <me@emilyhorsman.com>
2017-05-10 10:38:16 -0400

    Render suggestions and add selections to value

--- common/static/js/autocomplete.js
+++ common/static/js/autocomplete.js
@@ -6,22 +6,82 @@ import axios from 'axios'
 class Autocomplete extends PureComponent {
 	constructor(props, ...args) {
 		super(props, ...args)
+
+		this.handleChange = this.handleChange.bind(this)
+
 		this.state = {
 			value: props.value,
+			input: {
+				value: '',
+			},
+			suggestions: [],
 		}
+	}
+
+	handleChange(event) {
+		const { value } = event.target
+		this.checkNewSuggestions(value)
+		this.setState({
+			input: {
+				...this.state.input,
+				value,
+			}
+		})
+	}
+
+	checkNewSuggestions(value) {
+		if (value === this.state.value) {
+			return
+		}
+
+		const params = { search: value }
+		axios.get('/api/v1/pages', { params })
+			.then(res => {
+				if (res.status !== 200) {
+					return
+				}
+
+				this.setState({
+					suggestions: res.data.pages
+				})
+			})
+	}
+
+	handleClick(suggestion) {
+		this.setState({
+			value: this.state.value.concat(suggestion),
+		})
 	}
 
 	render() {
 		const { name } = this.props
-		const { value } = this.state
+		const { value, input, suggestions } = this.state
 
 		return (
 			<span>
 				<input
 					type="hidden"
-					value={value}
+					value={JSON.stringify(value)}
 					name={name}
 				/>
+
+				<input
+					type="text"
+					onChange={this.handleChange}
+					{...input}
+				/>
+
+				<ul>
+					{suggestions.map(suggestion =>
+						<li
+							key={suggestion.id}
+							onClick={this.handleClick.bind(this, suggestion)}
+						>
+							{suggestion.title}
+						</li>
+					)}
+				</ul>
+
 				{value.map(page =>
 					<div key={page.id}>{page.title}</div>
 				)}
