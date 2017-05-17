2b7465a0ca225f70b5e91f697e11c3266201296a
Emily Horsman <me@emilyhorsman.com>
2017-05-17 14:42:34 -0700

    Add create action and protect them

--- autocomplete/views.py
+++ autocomplete/views.py
@@ -1,5 +1,7 @@
 from django.apps import apps
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.views.decorators.http import require_GET, require_POST
 
 
 def render_page(page):
@@ -11,6 +13,8 @@ def render_page(page):
     return dict(id=page.id, label=label)
 
 
+@require_GET
+@login_required
 def search(request):
     search_query = request.GET.get('query', '')
     type = request.GET.get('type', 'wagtailcore.Page')
@@ -18,3 +22,10 @@ def search(request):
     queryset = model.objects.filter(title__icontains=search_query).live()[:20]
     results = map(render_page, queryset)
     return JsonResponse(dict(pages=list(results)))
+
+
+@require_POST
+@login_required
+def create(request):
+    type = request.POST.get('type', 'wagtailcore.Page')
+    return JsonResponse(0)
