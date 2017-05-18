f36c8f9dd19aab75e98f88b11c58bdb08eb42965
Michael Sheinberg <mike@freedom.press>
2017-05-18 19:24:23 -0400

    Fix tests for CircleCI
    Really this was an issue of trying to access ports remotely that are not
    accessible through the CI environment. Instead if in that situation, just poll
    from the remote docker instance using a remote command.

--- devops/tests/test_basic.py
+++ devops/tests/test_basic.py
@@ -1,17 +1,28 @@
 import ssl
+import os
 import urllib2 as urllib
 
 
-def test_mainpage():
+def test_mainpage(Command):
     """
     Basic test to make sure home-page is coming up.
     """
-    # Disable SSL verification - we are using self-signed certs here people!
-    context = ssl.create_default_context()
-    context.check_hostname = False
-    context.verify_mode = ssl.CERT_NONE
+    SITE_STRING = "Press Freedom Incidents"
 
-    request = urllib.urlopen('https://localhost:4443', context=context)
+    # When running under CI, the docker environment is remote and ports are
+    # readily accessible remotely. In that case poll from the docker image
+    # instead of from the host
+    if os.environ.get("CIRCLECI", "false") != "true":
+        # Disable SSL verification - we are using self-signed certs here people!
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
 
-    assert request.getcode() == 200
-    assert "Press Freedom Incidents" in str(request.read())
+        request = urllib.urlopen('https://localhost:4443', context=context)
+        assert request.getcode() == 200
+        assert SITE_STRING in str(request.read())
+    else:
+        content = Command.check_output("curl -k https://localhost:443")
+        head = Command.check_output("curl -I -k https://localhost:443")
+        assert "Press Freedom Incidents" in content
+        assert "HTTP/1.1 200 OK" in head
