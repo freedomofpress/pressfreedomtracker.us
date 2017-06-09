6f46e905aae00b10a3d9d89e9833b64c8764d7ef
Cameron Higby-Naquin <me@chigby.org>
2017-06-09 16:19:01 -0400

    Add Dark Gray as a color choice
    Note: eventually we will want to have these be tied to BEM modifiers,
    but for now let's just enumerate all the colors we want.

--- home/models.py
+++ home/models.py
@@ -89,6 +89,7 @@ class StatBox(Orderable):
             ('#5b9932', 'Green'),
             ('#803e79', 'Purple'),
             ('#dc810b', 'Orange'),
+            ('#2d2e2e', 'Dark Gray'),
         ],
     )
     link = models.ForeignKey(
