73ade64eee486d52c8a0ddafc5836b36fbe95ddb
Emily Horsman <me@emilyhorsman.com>
2017-06-12 14:53:12 -0700

    Add monotone pillow operation and run in MonotoneOperation

--- common/image_operations.py
+++ common/image_operations.py
@@ -1,4 +1,22 @@
 from wagtail.wagtailimages.image_operations import Operation
+from willow.registry import registry
+from willow.plugins.pillow import PillowImage
+
+
+# `L` is 8-bit pixels, black and white.
+PILLOW_MONOTONE_MODE = 'L'
+PILLOW_MONOTONE_MODE_WITH_ALPHA = 'LA'
+
+
+def pillow_monotone(image):
+    if image.has_alpha():
+        mode = PILLOW_MONOTONE_MODE_WITH_ALPHA
+    else:
+        mode = PILLOW_MONOTONE_MODE
+    return PillowImage(image.image.convert(mode))
+
+
+registry.register_operation(PillowImage, 'monotone', pillow_monotone)
 
 
 class MonotoneOperation(Operation):
@@ -6,4 +24,5 @@ class MonotoneOperation(Operation):
         pass
 
     def run(self, willow, image, env):
-        pass
+        willow = willow.monotone()
+        return willow
