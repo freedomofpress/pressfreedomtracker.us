5c25cb935abb122ac96b4c15ed7e8be262cfe9bd
Michael Sheinberg <mike@freedom.press>
2017-06-12 21:47:22 -0400

    expand CI script output, add sass-linter
    The sass-linter logic in particular requires some explanation since it looks
    really dumb. Basically, the first call (with the tail -f /dev/null) at the end
    is a hack to keep the docker image running in the background. From there we copy
    the current git repo code to the remote docker container. Finally, we execute
    the sass-lint script -- if that call fails it should return an non-zero exit
    code and crash the script

--- devops/scripts/go.sh
+++ devops/scripts/go.sh
@@ -2,19 +2,33 @@
 #
 #
 
+set -e
+
 if [ ! -f devops/.venv/bin/activate ]; then virtualenv --no-site-packages devops/.venv; fi
 source devops/.venv/bin/activate
 
 cd devops || exit
-pip install -U -r requirements.txt
+echo "##### Install pip requirements"
+pip install -U -r requirements.txt 1> /dev/null
 
 # Install external ansible role dependencies
-ansible-galaxy install -r requirements.yml
+echo "##### Install ansible galaxy requirements"
+ansible-galaxy install -r requirements.yml &> /dev/null
 
-docker pull msheiny/debian-jessie-systemd:latest
+echo "##### Run sass linter"
+# Start sasslinter container, have to get a little hacky since its running on a
+# remote docker instance
+docker run -d --entrypoint=/bin/ash --name sasslint quay.io/freedomofpress/sasslinter -c 'tail -f /dev/null'
+echo "Copying git directory to remote docker container..."
+docker cp "${PWD}/../" sasslint:/lintme
+docker exec -it sasslint ash -c 'cd /lintme && /usr/local/bin/sass-lint'
+
+echo "##### Run Debian playbook"
+docker pull msheiny/debian-jessie-systemd:latest &> /dev/null
 if [ "$1" != "only_tests" ]; then
     molecule create
     molecule converge
 fi
 
+echo "##### Run Testinfra pieces"
 testinfra --connection=docker --hosts=prod tests/
