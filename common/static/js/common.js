f1325ebb8d796a7c4413a86f92e4829fdd0fcf9d
Emily Horsman <me@emilyhorsman.com>
2017-06-14 11:26:25 -0700

    Add axios and configure default headers for django is_ajax

--- common/static/js/common.js
+++ common/static/js/common.js
@@ -1,4 +1,8 @@
 import classlist from 'classlist-polyfill'
 import 'normalize-css/normalize.css'
+import axios from 'axios'
+
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
 import '../css/common.sass'
 import '~/slidingnav'
