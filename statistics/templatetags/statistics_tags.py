a5bedcb025c5d45662b23c39690d4a1de0c01209
Cameron Higby-Naquin <me@chigby.org>
2017-06-15 10:55:07 -0400

    Include example of a statistics tag that returns a dict
    Something that others can modify or build from.  Also this is
    something that can be plugged into a "graph" or "visual" statistics
    representation (to be built later).

--- statistics/templatetags/statistics_tags.py
+++ statistics/templatetags/statistics_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+from django.db.models.functions import TruncMonth
 
 from incident.models.incident_page import IncidentPage
 
@@ -13,3 +15,28 @@ def num_incidents(year):
         live=True,
         date__year=year,
     ).count()
+
+
+@register.simple_tag
+def incidents_in_year_range_by_month(start_year, end_year):
+    data = (
+        IncidentPage.objects
+        .filter(
+            date__year__gte=start_year,
+            date__year__lte=end_year,
+        )
+        .annotate(month=TruncMonth('date'))
+        .values('month')
+        .annotate(c=Count('*')).order_by()
+    )
+    return { i['month'].strftime('%B %Y'): i['c'] for i in data }
+
+
+def as_table():
+    pass
+
+def as_histogram():
+    pass
+
+def as_statbox():
+    pass
