9679feb52b6335853e727c4aab659abf117a1d62
Michael Sheinberg <mike@freedom.press>
2017-06-15 18:52:59 -0400

    Enable paxcltd in docker image, fix for molecule
    So two problems cropped up that this "fixes" (waiting on CI run but tested
    successfully locally):
    1. molecule needs a tagged image but by pulling the image from a digest it was
    dropping it locally without a tag. The last bit of the go.sh script tags the www
    image accordingly.
    
    2. realized that paxctld wasnt enabled in the www image which broke local
    testing for folks with grsec kernels. docker image has been rebuilt and pushed
    and digest updated.

--- devops/scripts/go.sh
+++ devops/scripts/go.sh
@@ -7,8 +7,6 @@ set -e
 curdir="$(dirname $(realpath "$0") )"
 
 source .docker_versions
-SASSLINT_IMAGE="quay.io/freedomofpress/sasslinter"
-
 
 if [ "$1" != "only_tests" ]; then
     echo "##### Run sass linter"
@@ -35,7 +33,9 @@ source "${curdir}/../.venv/bin/activate"
 cd "$(dirname $(dirname $(realpath $0) )../)"|| exit
 
 echo "##### Pull Docker Image"
-docker pull "quay.io/freedomofpress/ci-webserver@sha256:${CIWWW_VER}" &> /dev/null
+docker pull "${CIWWW_IMAGE}@sha256:${CIWWW_VER}" &> /dev/null
+# molecule needs a tag to import
+docker tag "${CIWWW_IMAGE}@sha256:${CIWWW_VER}" "${CIWWW_IMAGE}:latest"
 echo "##### Run Django provision"
 if [ "$1" != "only_tests" ]; then
     molecule create
