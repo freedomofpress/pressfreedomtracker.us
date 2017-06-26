4e57129a2325ef06ad10b8fd60c0a72479aa893c
Emily Horsman <me@emilyhorsman.com>
2017-06-26 16:26:48 -0700

    Styles for single autocomplete selection

--- autocomplete/static/js/autocomplete.js
+++ autocomplete/static/js/autocomplete.js
@@ -196,16 +196,18 @@ class Single extends PureComponent {
 
 		if (selected) {
 			return (
-				<span>
-					{selected.label}
+				<div className="selection selection--single">
+					<span className="selection__label">{selected.label}</span>
 
 					<button
 						type="button"
+						className="selection__button"
 						onClick={onClick.bind(null, null)}
 					>
-						Remove
+						<svg className="selection__icon" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z"/></svg>
+						<span className="sr-only">Remove</span>
 					</button>
-				</span>
+				</div>
 			)
 		}
 
