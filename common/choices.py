6aa03771b5b36ad9b92c79275ed6eb5fc77ff0f1
racheltstevens <racheltstevens@gmail.com>
2017-07-04 15:34:20 -0400

    Remove inline styles
    Changes colors to use a map and classes
    More flexible control based on background color

--- common/choices.py
+++ common/choices.py
@@ -1,8 +1,8 @@
 COLOR_CHOICES=[
-    ('#119abc', 'Blue'),
-    ('#5b9932', 'Green'),
-    ('#803e79', 'Purple'),
-    ('#dc810b', 'Orange'),
-    ('#2d2e2e', 'Dark Gray'),
-    ('#fff', 'White')
+    ('blue', 'Blue'),
+    ('green', 'Green'),
+    ('purple', 'Purple'),
+    ('orange', 'Orange'),
+    ('dark-gray', 'Dark Gray'),
+    ('white', 'White')
 ]
