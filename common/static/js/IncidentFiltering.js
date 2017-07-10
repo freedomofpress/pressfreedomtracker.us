32cbff48d0567610aa064febb44707d4ecc2bc04
Emily Horsman <me@emilyhorsman.com>
2017-07-10 15:44:43 -0700

    Flesh out IncidentFiltering component, mainly header

--- common/static/js/IncidentFiltering.js
+++ common/static/js/IncidentFiltering.js
@@ -1,12 +1,85 @@
 import React, { PureComponent } from 'react'
 
 
-class IncidentFiltering extends PureComponent {
+function Filters({ children }) {
+	return <div className="filters">{children}</div>
+}
+
+
+class FiltersHeader extends PureComponent {
 	render() {
+		const {
+			filtersExpanded,
+			filtersApplied,
+			handleToggle
+		} = this.props
+
 		return (
-			<div>
-				hi
+			<div className="filters__header">
+				{filtersApplied.length === 0 && (
+					<div className="filters__text filters__text--dim">
+						No filters applied.
+					</div>
+				)}
+
+				{filtersApplied.length > 0 && (
+					<div className="filters__text">
+						Filters
+					</div>
+				)}
+
+				<button
+					className="filters__button"
+					onClick={handleToggle}
+				>
+					{filtersExpanded ? 'Collapse Filters' : 'Change Filters'}
+				</button>
 			</div>
+		)
+	}
+}
+
+
+class IncidentFiltering extends PureComponent {
+	constructor(props, ...args) {
+		super(props, ...args)
+
+		this.handleToggle = this.handleToggle.bind(this)
+
+		this.state = {
+			filtersEnabled: props.availableCategories.map(category => {
+				return {
+					...category,
+					enabled: false,
+				}
+			}),
+
+			filtersExpanded: false,
+
+			filtersApplied: [],
+		}
+	}
+
+	handleToggle() {
+		this.setState({
+			filtersExpanded: !this.state.filtersExpanded,
+		})
+	}
+
+	render() {
+		const {
+			filtersApplied,
+			filtersExpanded,
+		} = this.state
+
+		return (
+			<Filters>
+				<FiltersHeader
+					filtersApplied={filtersApplied}
+					filtersExpanded={filtersExpanded}
+					handleToggle={this.handleToggle}
+				/>
+			</Filters>
 		)
 	}
 }
