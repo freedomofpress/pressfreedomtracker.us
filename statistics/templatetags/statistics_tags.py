2cd0be1a52c28da6b0d61bf3b1655c2d10a82d68
Cameron Higby-Naquin <me@chigby.org>
2017-07-12 16:25:43 -0400

    Separate statistics dataset functions into numbers and maps
    Since it's seeming like we will want to use these two types of
    functions in different places on the site, it makes sense to
    conceptually separate numbers (that return a single number value) from
    maps (which return a list of pairs).
    
    This slightly changes the API for defining statistics, but I think
    ultimate it makes it better.

--- statistics/templatetags/statistics_tags.py
+++ statistics/templatetags/statistics_tags.py
@@ -3,13 +3,14 @@ from django.db.models import Count
 from django.db.models.functions import TruncMonth
 
 from incident.models.incident_page import IncidentPage
-from statistics.registry import statistic, get_stats
+from statistics.registry import Statistics
 
 
 register = template.Library()
+statistics = Statistics()
 
 
-@statistic
+@statistics.number
 @register.simple_tag
 def num_incidents(year):
     """Return the count of incidents occurring in the given year"""
@@ -19,7 +20,7 @@ def num_incidents(year):
     ).count()
 
 
-@statistic
+@statistics.map
 @register.simple_tag
 def incidents_in_year_range_by_month(start_year, end_year):
     """Return a list of (Month, Count) pairs for incidents within timespan
@@ -42,8 +43,8 @@ def incidents_in_year_range_by_month(start_year, end_year):
     return [(i['month'].strftime('%B %Y'), i['c']) for i in data]
 
 
-@register.simple_tag
-def display_stat(statistic, params):
-    """Not totally sure this is needed."""
-    stats = get_stats()
-    return stats[statistic](*params.split())
+# @register.simple_tag
+# def display_stat(statistic, params):
+#     """Not totally sure this is needed."""
+#     stats = get_stats()
+#     return stats[statistic](*params.split())
