ce9c237b1d739c07c7ee872cb57e8e41f220626e
Emily Horsman <me@emilyhorsman.com>
2017-07-14 11:34:46 -0700

    Add IncidentFilter.from_request

--- incident/models/incident_index_page.py
+++ incident/models/incident_index_page.py
@@ -50,57 +50,7 @@ class IncidentIndexPage(RoutablePageMixin, Page):
     def get_context(self, request):
         context = super(IncidentIndexPage, self).get_context(request)
 
-        incident_filter = IncidentFilter(
-            search_text=request.GET.get('search'),
-            lower_date=request.GET.get('lower_date'),
-            upper_date=request.GET.get('upper_date'),
-            categories=request.GET.get('categories'),
-            targets=request.GET.get('targets'),
-            affiliation=request.GET.get('affiliation'),
-            states=request.GET.get('states'),
-            tags=request.GET.get('tags'),
-            # ARREST / DETENTION
-            arrest_status=request.GET.get('arrest_status'),
-            status_of_charges=request.GET.get('status_of_charges'),
-            current_charges=request.GET.get('current_charges'),
-            dropped_charges=request.GET.get('dropped_charges'),
-            # EQUIPMENT
-            equipment_seized=request.GET.get('equipment_seized'),
-            equipment_broken=request.GET.get('equipment_broken'),
-            status_of_seized_equipment=request.GET.get('status_of_seized_equipment'),
-            is_search_warrant_obtained=request.GET.get('is_search_warrant_obtained'),
-            actor=request.GET.get('actors'),
-            # BORDER STOP
-            border_point=request.GET.get('border_point'),
-            stopped_at_border=request.GET.get('stopped_at_border'),
-            target_us_citizenship_status=request.GET.get('target_us_citizenship_status'),
-            denial_of_entry=request.GET.get('denial_of_entry'),
-            stopped_previously=request.GET.get('stopped_previously'),
-            target_nationality=request.GET.get('target_nationality'),
-            did_authorities_ask_for_device_access=request.GET.get('did_authorities_ask_for_device_access'),
-            did_authorities_ask_for_social_media_user=request.GET.get('did_authorities_ask_for_social_media_user'),
-            did_authorities_ask_for_social_media_pass=request.GET.get('did_authorities_ask_for_social_media_pass'),
-            did_authorities_ask_about_work=request.GET.get('did_authorities_ask_about_work'),
-            were_devices_searched_or_seized=request.GET.get('weredevices_searched_or_seized'),
-            # PHYSICAL ASSAULT
-            assailant=request.GET.get('assailant'),
-            was_journalist_targeted=request.GET.get('was_journalist_targeted'),
-            # LEAK PROSECUTION
-            charged_under_espionage_act=request.GET.get('charged_under_espionage_act'),
-            # SUBPOENA
-            subpoena_subject=request.GET.get('subpoena_subject'),
-            subpoena_type=request.GET.get('subpoena_type'),
-            subpoena_status=request.GET.get('subpoena_status'),
-            held_in_contempt=request.GET.get('held_in_contempt'),
-            detention_status=request.GET.get('detention_status'),
-            third_party_in_possession_of_communications=request.GET.get('third_party_in_possession_of_communications'),
-            third_party_business=request.GET.get('third_party_business'),
-            legal_order_type=request.GET.get('legal_order_type'),
-            # PRIOR RESTRAINT
-            status_of_prior_restraint=request.GET.get('status_of_prior_restraint'),
-            # DENIAL OF ACCESS
-            politicians_or_public_figures_involved=request.GET.get('politicians_or_public_figures_involved'),
-        )
+        incident_filter = IncidentFilter.from_request(request)
         context['category_options'] = incident_filter.get_category_options()
         entry_qs = incident_filter.fetch()
 
