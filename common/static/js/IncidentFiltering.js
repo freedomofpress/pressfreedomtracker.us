2ade5d2b15a12a7c446dd52eaeca89af1baa865e
Emily Horsman <me@emilyhorsman.com>
2017-07-19 14:43:27 -0700

    Switch to semicolon as category summary delimiter

--- common/static/js/IncidentFiltering.js
+++ common/static/js/IncidentFiltering.js
@@ -56,7 +56,7 @@ function CategoryList({ categories }) {
 	return (
 		<ul className="filters__summary-list">
 			{categories.map(category => (
-				<li key={category.id} className="filters__summary-item">
+				<li key={category.id} className="filters__summary-item filters__summary-item--semicolons">
 					{category.title}
 				</li>
 			))}
