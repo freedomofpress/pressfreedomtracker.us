7569f08d4cce5e8ac9a9b1f6abd0053c91269365
Emily Horsman <me@emilyhorsman.com>
2017-07-21 15:13:15 -0700

    Refactor IncidentFiltering into submodules

---
+++ common/static/js/filtering/constants.js
@@ -0,0 +1,83 @@
+export const HUMAN_DATE_FORMAT = 'MMM Do YYYY'
+
+
+export const DATE_FORMAT = 'YYYY-MM-DD'
+
+
+export const AUTOCOMPLETE_MULTI_FILTERS = [
+	'targets',
+	'tags',
+	'current_charges',
+	'dropped_charges',
+	'equipment_seized',
+	'equipment_broken',
+	'politicians_or_public_figures_involved',
+	'target_nationality',
+]
+
+
+export const AUTOCOMPLETE_SINGLE_FILTERS = [
+	'state',
+	'venue',
+]
+
+
+export const DATE_FILTERS = [
+	'lower_date',
+	'upper_date',
+	'detention_date_lower',
+	'detention_date_upper',
+	'release_date_lower',
+	'release_date_upper',
+]
+
+
+export const ALL_FILTERS = [
+	'lower_date',
+	'upper_date',
+	'arrest_status',
+	'status_of_charges',
+	'current_charges',
+	'dropped_charges',
+	'detention_date_lower',
+	'detention_date_upper',
+	'release_date_lower',
+	'release_date_upper',
+	'unnecessary_use_of_force',
+	'border_point',
+	'stopped_at_border',
+	'stopped_previously',
+	'target_us_citizenship_status',
+	'denial_of_entry',
+	'target_nationality',
+	'did_authorities_ask_for_device_access',
+	'did_authorities_ask_for_social_media_user',
+	'did_authorities_ask_for_social_media_pass',
+	'did_authorities_ask_about_work',
+	'were_devices_searched_or_seized',
+	'politicians_or_public_figures_involved',
+	'equipment_seized',
+	'equipment_broken',
+	'status_of_seized_equipment',
+	'is_search_warrant_obtained',
+	'actor',
+	'affiliation',
+	'city',
+	'state',
+	'targets',
+	'tags',
+	'lawsuit_name',
+	'venue',
+	'charged_under_espionage_act',
+	'third_party_in_possession_of_communications',
+	'third_party_business',
+	'legal_order_type',
+	'assailant',
+	'was_journalist_targeted',
+	'status_of_prior_restraint',
+	'subpoena_subject',
+	'subpoena_type',
+	'subpoena_status',
+	'held_in_contempt',
+	'detention_status',
+]
