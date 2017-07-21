cae55fc52b6b40e1b9df15cdadbb6e7643edab4b
nmorduch <nmorduch@gmail.com>
2017-07-21 11:23:11 -0400

    Have list-comma-separated take a variable for punctuation
    Oops I got overly clever with this yay mixins

--- common/static/js/IncidentFiltering.js
+++ common/static/js/IncidentFiltering.js
@@ -56,7 +56,7 @@ function CategoryList({ categories }) {
 	return (
 		<ul className="filters__summary-list">
 			{categories.map(category => (
-				<li key={category.id} className="filters__summary-item filters__summary-item--semicolons">
+				<li key={category.id} className="filters__summary-item">
 					{category.title}
 				</li>
 			))}
