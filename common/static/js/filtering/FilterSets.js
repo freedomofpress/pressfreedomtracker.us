bc5dedd47a1dd27e460f014178ee73247eb182df
Emily Horsman <me@emilyhorsman.com>
2017-07-24 11:54:40 -0700

    Fix typo in FilterSets input
    Change from British to American spelling of "targeted"

--- common/static/js/filtering/FilterSets.js
+++ common/static/js/filtering/FilterSets.js
@@ -65,7 +65,7 @@ FilterSets['General'] = function({ handleFilterChange, filterValues }) {
 			<AutocompleteInput
 				handleFilterChange={handleFilterChange}
 				filterValues={filterValues}
-				label="Targetted any of these journalists"
+				label="Targeted any of these journalists"
 				filter="targets"
 				type="incident.Target"
 				isSingle={false}
