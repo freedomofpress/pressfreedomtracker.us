83f0a7e4139e7c3cd777ab8aa52b5ad9e2ce455c
Emily Horsman <me@emilyhorsman.com>
2017-07-24 15:47:24 -0700

    Add customizable button text to FormPage

--- forms/models.py
+++ forms/models.py
@@ -1,3 +1,4 @@
+from django.db import models
 from modelcluster.fields import ParentalKey
 from wagtail.wagtailadmin.edit_handlers import (
     FieldPanel, FieldRowPanel,
@@ -14,11 +15,17 @@ class FormField(AbstractFormField):
 class FormPage(AbstractEmailForm):
     intro = RichTextField(blank=True)
     thank_you_text = RichTextField(blank=True)
+    button_text = models.CharField(
+        max_length=30,
+        blank=True,
+        null=True,
+    )
 
     content_panels = AbstractEmailForm.content_panels + [
         FieldPanel('intro', classname="full"),
         InlinePanel('form_fields', label="Form fields"),
         FieldPanel('thank_you_text', classname="full"),
+        FieldPanel('button_text'),
         MultiFieldPanel([
             FieldRowPanel([
                 FieldPanel('from_address', classname="col6"),
