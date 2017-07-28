95452546e3832d4235721cb94cf7e43edf19b1f3
Emily Horsman <me@emilyhorsman.com>
2017-07-27 17:15:40 -0700

    Basic functionality for EmailSignup component

---
+++ common/static/js/emails/EmailSignup.js
@@ -0,0 +1,120 @@
+import React, { PureComponent } from 'react'
+import axios from 'axios'
+
+
+class EmailSignup extends PureComponent {
+	constructor(props, ...args) {
+		super(props, ...args)
+
+		this.handleSubmit = this.handleSubmit.bind(this)
+
+		this.state = {
+			email_address: '',
+			state: EmailSignup.INPUT,
+		}
+	}
+
+	handleChange(field, event) {
+		this.setState({ [field]: event.target.value })
+	}
+
+	handleSubmit(event) {
+		event.preventDefault()
+
+		const {
+			INPUT,
+			LOADING,
+			ALREADY_SIGNED_UP,
+			SUCCESS,
+		} = EmailSignup
+
+		this.setState({ state: LOADING })
+
+		const handleResolve = (response) => {
+			if (response.status === 200) {
+				var state = SUCCESS
+			} else {
+				var state = INPUT
+			}
+
+			this.setState({ state })
+		}
+
+		const handleReject = (error) => {
+			if (error.response.data === 'already_signed_up') {
+				var state = ALREADY_SIGNED_UP
+			} else {
+				var state = INPUT
+			}
+			this.setState({ state })
+		}
+
+		axios.post(
+			'/emails/create/',
+			`email_address=${this.state.email_address}`
+		).then(handleResolve, handleReject)
+	}
+
+	render() {
+		const {
+			INPUT,
+			LOADING,
+			ALREADY_SIGNED_UP,
+			SUCCESS,
+		} = EmailSignup
+
+		const {
+			state,
+			email_address,
+		} = this.state
+
+		if (state === ALREADY_SIGNED_UP) {
+			return (
+				<div className="email-signup email-signup--already-signed-up">
+					<p>This email address is already signed up.</p>
+				</div>
+			)
+		}
+
+		if (state === SUCCESS) {
+			return (
+				<div className="email-signup email-signup--success">
+					<p>Success!</p>
+				</div>
+			)
+		}
+
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<input
+					type="text"
+					value={email_address}
+					onChange={this.handleChange.bind(this, 'email_address')}
+					disabled={state !== INPUT}
+				/>
+
+				<button
+					type="submit"
+					disabled={state !== INPUT}
+				>
+					Signup
+				</button>
+			</form>
+		)
+	}
+}
+
+
+EmailSignup.INPUT = 'input'
+
+
+EmailSignup.LOADING = 'loading'
+
+
+EmailSignup.ALREADY_SIGNED_UP = 'already_signed_up'
+
+
+EmailSignup.SUCCESS = 'success'
+
+
+export default EmailSignup
