1c64fedf30aa8ee8b6ccab9a10a0d3cc0d5cb264
Harris Lapiroff <harris@chromamine.com>
2017-08-01 23:43:22 -0400

    Count arrests in a more definitive way
    Now that arrests is its own category, we can use that category's
    ID to definitively identify what is or isn't an arrest

---
+++ statistics/utils.py
@@ -0,0 +1,14 @@
+# IDs of CategoryPage objects
+# These IDs are tied to the database. It's not ideal, but some of the marriage
+# of function and content on this site requires that categories be represented
+# in the code. If the categories ever change in the database, they will also
+# need to be updated here.
+ARREST_ID = 4
+BORDER_STOP_ID = 5
+SUBPOENA_ID = 6
+LEAK_ID = 7
+EQUIPMENT_SEARCH_ID = 9
+PHYSICAL_ATTACK_ID = 10
+DENIAL_OF_ACCESS_ID = 52
+CHILLING_STATEMENT_ID = 62
+OTHER_ID = 63
