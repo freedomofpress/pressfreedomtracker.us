b2f19906ac73bf760caa3287a91400c6daafe765
Harris Lapiroff <harris@chromamine.com>
2017-08-03 13:57:17 -0700

    Purge incident page caches when related incidents change

--- incident/signals.py
+++ incident/signals.py
@@ -17,7 +17,31 @@ def purge_incident_from_frontend_cache_for_category(
         purge_page_from_cache(incident)
 
 
+def purge_incident_from_frontend_cache_for_incident(
+    instance=None, sender=None, **kwargs
+):
+    """
+    Busts the cache for any incident that shares a category or is
+    a related incident to the incident just changed.
+    """
+
+    # Purge cache for related incidents
+    for incident in instance.related_incidents.all():
+        purge_page_from_cache(incident)
+
+    # Purge cache for incidents that share a category and may
+    # therefore show up as a related incident
+    for categorization in instance.categories.all():
+        purge_incident_from_frontend_cache_for_category(
+            instance=categorization.category
+        )
+
+
 post_save.connect(
     purge_incident_from_frontend_cache_for_category,
     sender=CategoryPage
+)
+post_save.connect(
+    purge_incident_from_frontend_cache_for_incident,
+    sender=IncidentPage
 )
--- incident/tests/test_frontend_cache_purge.py
+++ incident/tests/test_frontend_cache_purge.py
@@ -8,6 +8,7 @@ from incident.tests.factories import IncidentPageFactory, IncidentCategorization
 @patch('incident.signals.purge_page_from_cache')
 class TestIncidentPageCachePurge(TestCase):
     def test_cache_purged_on_category_change(self, purge_page_from_cache):
+        "Should purge cache for an incident in a category when category changes"
         category = CategoryPageFactory()
         incident = IncidentPageFactory()
         categorization = IncidentCategorizationFactory(
@@ -25,3 +26,75 @@ class TestIncidentPageCachePurge(TestCase):
         category.save()  # Should trigger purge on incident page
 
         purge_page_from_cache.assert_called_with(incident)
+
+    def test_cache_not_purged_on_wrong_category_change(
+        self,
+        purge_page_from_cache
+    ):
+        """
+        Should not purge cache for an incident when a category changes if the
+        incident is not in that category.
+        """
+
+        category = CategoryPageFactory()
+        incident = IncidentPageFactory()
+
+        category.title = "New Category Name"
+        category.save()  # Should NOT trigger purge on incident page
+
+        self.assertFalse(purge_page_from_cache.called)
+
+    def test_cache_purged_on_related_incident(self, purge_page_from_cache):
+        "Should purge cache for an incident when a related incident changes"
+        incident1 = IncidentPageFactory()
+        incident2 = IncidentPageFactory(related_incidents=[incident1])
+
+        incident1.title = 'New Incident Name'
+        incident1.save()  # Should trigger purge on incident2
+
+        purge_page_from_cache.assert_called_with(incident2)
+
+    def test_cache_not_purged_on_unrelated_incident(
+        self,
+        purge_page_from_cache
+    ):
+        """
+        Should not purge cache for an incident when another unrelated incident
+        changes
+        """
+        incident1 = IncidentPageFactory()
+        incident2 = IncidentPageFactory()
+
+        incident1.title = 'New Incident Name'
+        incident1.save()  # Should trigger purge on incident2
+
+        self.assertFalse(purge_page_from_cache.called)
+
+    def test_cache_purged_on_same_cat_incident(
+        self,
+        purge_page_from_cache
+    ):
+        """
+        Should purge cache for an incident that is in the same category
+        as a changed incident
+        """
+        category = CategoryPageFactory()
+        incident1 = IncidentPageFactory()
+        incident2 = IncidentPageFactory()
+        categorization1 = IncidentCategorizationFactory(
+            incident_page=incident1,
+            category=category
+        )
+        categorization2 = IncidentCategorizationFactory(
+            incident_page=incident2,
+            category=category
+        )
+        # Not sure why, but the above doesn't commit this to the db
+        # without the next lines:
+        categorization1.save()
+        categorization2.save()
+
+        incident1.title = 'New Incident Title'
+        incident1.save()
+
+        purge_page_from_cache.assert_called_with(incident2)
