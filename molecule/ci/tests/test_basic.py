3f090d2e2dca7ee29ff0e04172b9478f4a3a4004
Michael Sheinberg <mike@freedom.press>
2017-09-15 15:59:01 -0700

    Address PEP8 issues in testinfra tests

--- molecule/ci/tests/test_basic.py
+++ molecule/ci/tests/test_basic.py
@@ -1,8 +1,3 @@
-import ssl
-import os
-import urllib2 as urllib
-
-
 def test_mainpage(host):
     """
     Basic test to make sure home-page is coming up.
@@ -11,5 +6,5 @@ def test_mainpage(host):
 
     content = host.check_output("curl -k https://localhost:443")
     head = host.check_output("curl -I -k https://localhost:443")
-    assert "Press Freedom Incidents" in content
+    assert SITE_STRING in content
     assert "HTTP/1.1 200 OK" in head
