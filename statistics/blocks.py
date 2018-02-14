d1f53456d7fedf75d59e74ec23125ceca3d9f8bc
Stephen Burrows <stephen.r.burrows@gmail.com>
2018-02-14 14:13:57 -0800

    Added validation of params to statistics block

--- statistics/blocks.py
+++ statistics/blocks.py
@@ -1,3 +1,7 @@
+from inspect import Parameter
+import inspect
+
+from django.core.exceptions import ValidationError
 from wagtail.wagtailcore import blocks
 
 from statistics.registry import get_stats, get_stats_choices
@@ -25,6 +29,37 @@ class StatisticsBlock(blocks.StructBlock):
         required=False,
         help_text='Whitespace-separated list of arguments to be passed to the statistics function',
     )
+
+    def clean(self, value):
+        cleaned_value = super(StatisticsBlock, self).clean(value)
+        errors = {}
+
+        fn = get_stats()[cleaned_value['dataset']]
+        params = cleaned_value['params'].split()
+
+        signature = inspect.signature(fn)
+        required_params = [param for param in signature.parameters.values() if param.default == Parameter.empty]
+
+        param_count = len(params)
+        min_param_count = len(required_params)
+        max_param_count = len(signature.parameters)
+
+        if param_count < min_param_count:
+            errors['params'] = ['At least {} parameter{} must be supplied for this dataset'.format(min_param_count, 's' if min_param_count != 1 else '')]
+
+        if param_count > max_param_count:
+            if max_param_count == 0:
+                errors['params'] = ['No parameters may be supplied for this dataset']
+            else:
+                errors['params'] = ['At most {} parameter{} may be supplied for this dataset'.format(max_param_count, 's' if max_param_count != 1 else '')]
+
+        if errors:
+            # The message here is arbitrary - StructBlock.render_form will suppress it
+            # and delegate the errors contained in the 'params' dict to the child blocks instead
+            # See wagtail/wagtailcore/blocks/struct_block.py line 116
+            raise ValidationError('Validation error in StructBlock', params=errors)
+
+        return cleaned_value
 
     def get_context(self, value, parent_context=None):
         context = super(StatisticsBlock, self).get_context(value, parent_context=parent_context)
