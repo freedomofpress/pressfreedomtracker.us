40a38504b42ee84316a315a47426242d24d7e333
Stephen Burrows <stephen.r.burrows@gmail.com>
2018-02-14 16:08:16 -0800

    Added template validator object

---
+++ common/validators.py
@@ -0,0 +1,42 @@
+from django.core.exceptions import ValidationError
+from django.template.base import Parser, Lexer
+from django.template.engine import Engine
+from django.template.exceptions import TemplateSyntaxError
+from django.template.library import import_library
+
+
+class TemplateValidator(object):
+    disallowed_tags = [
+        'load',
+        'extends',
+        'include',
+        'debug',
+        'csrf_token',
+    ]
+    libraries = Engine.default_builtins + [
+        'statistics.templatetags.statistics_tags',
+    ]
+
+    def _disallowed_tag(self, *args, **kwargs):
+        raise TemplateSyntaxError('{} tags are not allowed'.format(', '.join(self.disallowed_tags)))
+
+    def __call__(self, value):
+        # Try to parse the template looking for syntax errors.
+        lexer = Lexer(value)
+        tokens = lexer.tokenize()
+        parser = Parser(
+            tokens,
+            builtins=[import_library(path) for path in self.libraries],
+        )
+
+        for tag in self.disallowed_tags:
+            parser.tags[tag] = self._disallowed_tag
+
+        error = None
+        try:
+            parser.parse()
+        except TemplateSyntaxError as exc:
+            error = str(exc)
+
+        if error:
+            raise ValidationError(error)
