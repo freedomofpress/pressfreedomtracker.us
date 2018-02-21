6d6466cb7f5da9a159cfd47274a03bc499db554f
racheltstevens <racheltstevens@gmail.com>
2018-02-21 11:54:43 -0500

    Appease flake8

--- common/tests/test_field_utils.py
+++ common/tests/test_field_utils.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from common.utils.fields import remove_unwanted_fields, get_field_tuple, get_incident_field_dict
+from common.utils.fields import remove_unwanted_fields, get_field_tuple
 from incident.models import IncidentPage
+
 
 class TestRemoveUnwantedFields(TestCase):
     def test_returns_true_if_field_is_valid(self):
@@ -16,17 +17,23 @@ class TestGetFieldTuple(TestCase):
     def test_field_with_verbose_name(self):
         field = IncidentPage._meta.get_field('arrest_status')
         expected = (field.name, field.verbose_name)
-        self.assertEqual(get_field_tuple(field), expected
-            )
+        self.assertEqual(
+            get_field_tuple(field),
+            expected
+        )
 
     def test_field_without_verbose_name(self):
         field = IncidentPage._meta.get_field('body')
         expected = (field.name, field.verbose_name)
-        self.assertEqual(get_field_tuple(field), expected
-            )
+        self.assertEqual(
+            get_field_tuple(field),
+            expected
+        )
 
     def test_field_with_related_name(self):
         field = IncidentPage._meta.get_field('updates')
         expected = (field.name, field.related_name)
-        self.assertEqual(get_field_tuple(field), expected
-            )
+        self.assertEqual(
+            get_field_tuple(field),
+            expected
+        )
