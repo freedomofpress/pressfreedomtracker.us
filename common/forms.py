78d00162eacbe02db387ed61e85224d21a49d274
racheltstevens <racheltstevens@gmail.com>
2018-03-09 14:23:56 -0500

    Refactor merge forms
    Create a base merge form that all other merge forms inherit from.
    Improve labels, make fields required.

--- common/forms.py
+++ common/forms.py
@@ -1,20 +1,31 @@
 from django import forms
+from django.utils.text import capfirst
 from common.models import CommonTag
 
 from autocomplete.widgets import Autocomplete
 
 
-class TagMergeForm(forms.Form):
+class BaseMergeForm(forms.Form):
+    title_for_merged_models = title_for_merged_models = forms.CharField(
+        max_length=255,
+        required=True
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['models_to_merge'] = forms.models.ModelMultipleChoiceField(
+                queryset=self.merge_model.objects.all(),
+                widget=type(
+                    '_Autocomplete',
+                    (Autocomplete,),
+                    dict(page_type=self.merge_model_type, can_create=False, is_single=False)
+                ),
+                label='{} to merge'.format(capfirst(self.merge_model_name))
+            )
+        self.fields['title_for_merged_models'].label = 'Title for merged {}'.format(self.merge_model_name)
+
+
+class TagMergeForm(BaseMergeForm):
     merge_model = CommonTag
     merge_model_type = 'common.CommonTag'
     merge_model_name = 'tags'
-    models_to_merge = forms.models.ModelMultipleChoiceField(
-        queryset=merge_model.objects.all(),
-        widget=type(
-            '_Autocomplete',
-            (Autocomplete,),
-            dict(page_type=merge_model_type, can_create=False, is_single=False)
-        ),
-        label='Fields to merge'
-    )
-    title_for_merged_models = forms.CharField(max_length=255, label='Title for merged {}'.format(merge_model_name))
--- incident/forms.py
+++ incident/forms.py
@@ -1,82 +1,35 @@
 from django import forms
+
+from common.forms import BaseMergeForm
 from incident.models import Target, Charge, Nationality, Venue, PoliticianOrPublic
 from autocomplete.widgets import Autocomplete
 
 
-class TargetMergeForm(forms.Form):
+class TargetMergeForm(BaseMergeForm):
     merge_model = Target
     merge_model_type = 'incident.Target'
     merge_model_name = 'targets'
-    models_to_merge = forms.models.ModelMultipleChoiceField(
-        queryset=merge_model.objects.all(),
-        widget=type(
-            '_Autocomplete',
-            (Autocomplete,),
-            dict(page_type=merge_model_type, can_create=False, is_single=False)
-        ),
-        label='Fields to merge'
-    )
-    title_for_merged_models = forms.CharField(max_length=255, label='Title for merged {}'.format(merge_model_name))
 
-class ChargeMergeForm(forms.Form):
+
+class ChargeMergeForm(BaseMergeForm):
     merge_model = Charge
     merge_model_type = 'incident.Charge'
     merge_model_name = 'charges'
-    models_to_merge = forms.models.ModelMultipleChoiceField(
-        queryset=merge_model.objects.all(),
-        widget=type(
-            '_Autocomplete',
-            (Autocomplete,),
-            dict(page_type=merge_model_type, can_create=False, is_single=False)
-        ),
-        label='Fields to merge'
-    )
-    title_for_merged_models = forms.CharField(max_length=255, label='Title for merged {}'.format(merge_model_name))
 
 
-class NationalityMergeForm(forms.Form):
+class NationalityMergeForm(BaseMergeForm):
     merge_model = Nationality
     merge_model_type = 'incident.Nationality'
     merge_model_name = 'nationalities'
-    models_to_merge = forms.models.ModelMultipleChoiceField(
-        queryset=merge_model.objects.all(),
-        widget=type(
-            '_Autocomplete',
-            (Autocomplete,),
-            dict(page_type=merge_model_type, can_create=False, is_single=False)
-        ),
-        label='Fields to merge'
-    )
-    title_for_merged_models = forms.CharField(max_length=255, label='Title for merged {}'.format(merge_model_name))
 
 
-class VenueMergeForm(forms.Form):
+class VenueMergeForm(BaseMergeForm):
     merge_model = Venue
     merge_model_type = 'incident.Venue'
     merge_model_name = 'venues'
-    models_to_merge = forms.models.ModelMultipleChoiceField(
-        queryset=merge_model.objects.all(),
-        widget=type(
-            '_Autocomplete',
-            (Autocomplete,),
-            dict(page_type=merge_model_type, can_create=False, is_single=False)
-        ),
-        label='Fields to merge'
-    )
-    title_for_merged_models = forms.CharField(max_length=255, label='Title for merged {}'.format(merge_model_name))
 
 
-class PoliticianOrPublicMergeForm(forms.Form):
+class PoliticianOrPublicMergeForm(BaseMergeForm):
     merge_model = PoliticianOrPublic
     merge_model_type = 'incident.PoliticianOrPublic'
     merge_model_name = 'politician or public figure'
-    models_to_merge = forms.models.ModelMultipleChoiceField(
-        queryset=merge_model.objects.all(),
-        widget=type(
-            '_Autocomplete',
-            (Autocomplete,),
-            dict(page_type=merge_model_type, can_create=False, is_single=False)
-        ),
-        label='Fields to merge'
-    )
-    title_for_merged_models = forms.CharField(max_length=255, label='Title for merged {}'.format(merge_model_name))
