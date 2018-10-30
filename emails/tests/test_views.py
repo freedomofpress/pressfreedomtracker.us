0f0143741afce724b8b012175240200f7aff87e5
Cameron Higby-Naquin <me@chigby.org>
2018-10-30 18:06:52 -0400

    Strip whitespace from email addresses before signing them up
    Also, add some very basic functionality tests for the email signup
    feature.

---
+++ emails/tests/test_views.py
@@ -0,0 +1,41 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+from emails.models import EmailSignup
+
+
+class EmailSignupTest(TestCase):
+    def test_successful_signup(self):
+        response = self.client.post(
+            reverse("email-signup-create"), {"email_address": "hello@example.com"}
+        )
+        self.assertEqual(response.status_code, 200)
+
+        # should not raise error
+        EmailSignup.objects.get(email_address="hello@example.com")
+
+    def test_should_reject_posts_without_email(self):
+        response = self.client.post(reverse("email-signup-create"), {})
+        self.assertEqual(response.status_code, 400)
+
+    def test_should_reject_non_email_addresses(self):
+        response = self.client.post(
+            reverse("email-signup-create"), {"email_address": "example.com"}
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_should_reject_already_taken_emails(self):
+        EmailSignup.objects.create(email_address="hello@example.com")
+        response = self.client.post(
+            reverse("email-signup-create"), {"email_address": "hello@example.com"}
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_signup_should_permit_leading_or_trailing_whitespace(self):
+        response = self.client.post(
+            reverse("email-signup-create"), {"email_address": "  hello@example.com    "}
+        )
+        self.assertEqual(response.status_code, 200)
+
+        # should not raise error
+        EmailSignup.objects.get(email_address="hello@example.com")
