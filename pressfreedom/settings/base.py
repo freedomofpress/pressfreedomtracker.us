692443793817753a81a0d00e264a4164a0a93639
Cameron Higby-Naquin <me@chigby.org>
2018-11-28 10:42:15 -0500

    Move client-side static files from `/common/static` to `/client`
    Prefer a top-level organization to a nested directory in a specific
    app.
    
    Refs #514

--- pressfreedom/settings/base.py
+++ pressfreedom/settings/base.py
@@ -153,6 +153,7 @@ STATICFILES_FINDERS = [
 
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(BASE_DIR, 'client'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
