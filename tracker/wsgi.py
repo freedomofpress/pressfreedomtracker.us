d1ea56b1decb436400a3fbf1d26af4504e0c339b
Cameron Higby-Naquin <me@chigby.org>
2018-12-10 14:51:25 -0500

    Rename `pressfreedom` to `tracker`
    Ultimately, this web site is about tracking press freedom incidents,
    so tracking is the focus.  Press freedom is what the organization as a
    whole is concerned with, so that name is a little ambiguous.

--- tracker/wsgi.py
+++ tracker/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pressfreedom project.
+WSGI config for press freedom tracker project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pressfreedom.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracker.settings.dev")
 
 application = get_wsgi_application()
