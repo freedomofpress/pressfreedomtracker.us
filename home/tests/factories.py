1826b0798a746edf2c90fd741d189999245063c8
Cameron Higby-Naquin <me@chigby.org>
2019-06-11 11:33:28 -0400

    factories: Enhance home page factory
    Adds more representative fake data.  Perhaps more important for
    usability is it will (a) have a default title of "Home" and (b) defer
    to pre-existing Home Pages with that title.  This means we can just
    call `HomePageFactory()` and it will do the right thing in most cases
    and we needn't worry about checking if one already exists.

--- home/tests/factories.py
+++ home/tests/factories.py
@@ -1,11 +1,19 @@
 import factory
 import wagtail_factories
+from faker import Faker
+from wagtail.core.rich_text import RichText
 
 from home.models import HomePage
+
+
+fake = Faker()
 
 
 class HomePageFactory(wagtail_factories.PageFactory):
     class Meta:
         model = HomePage
+        django_get_or_create = ('slug',)
 
-    title = factory.Sequence(lambda n: 'Home {n}'.format(n=n))
+    title = 'Home'
+    about = factory.LazyAttribute(lambda _: RichText(fake.paragraph(nb_sentences=10)))
+
