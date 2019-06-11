1e437945e44aaf487e8bdc425700bef60397ab49
Cameron Higby-Naquin <me@chigby.org>
2019-06-11 11:35:31 -0400

    factories: Add statbox factory

--- home/tests/factories.py
+++ home/tests/factories.py
@@ -3,7 +3,8 @@ import wagtail_factories
 from faker import Faker
 from wagtail.core.rich_text import RichText
 
-from home.models import HomePage
+from home.models import HomePage, StatBox
+from common.choices import CATEGORY_COLOR_CHOICES
 
 
 fake = Faker()
@@ -17,3 +18,27 @@ class HomePageFactory(wagtail_factories.PageFactory):
     title = 'Home'
     about = factory.LazyAttribute(lambda _: RichText(fake.paragraph(nb_sentences=10)))
 
+
+class StatBoxFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = StatBox
+
+    class Params:
+        # By default, no category, but you may supply one to
+        # auto-populate other fields below.
+        category = None
+
+    sort_order = factory.Sequence(lambda n: n)
+    value = factory.LazyAttribute(
+        lambda o: '{{% num_incidents categories={} %}}'.format(o.category.pk) if o.category else '{% num_incidents %}'
+    )
+    label = factory.LazyAttribute(
+        lambda o: 'Total {}'.format(o.category.plural_name) if o.category else '{% num_incidents %}'
+    )
+
+    color = factory.Iterator(CATEGORY_COLOR_CHOICES, getter=lambda c: c[0])
+    internal_link = factory.LazyAttribute(
+        lambda o: o.category if o.category else None
+    )
+    querystring = ''
+    external_link = '/'
