0d4832c9a9d19f3bd1551ed7a78069867be50877
Cameron Higby-Naquin <me@chigby.org>
2020-01-03 11:28:42 -0500

    Fix deprecated `SearchForm` import path
    This will be changed in wagtail 2.5, according to the warning, so
    let's move it.

--- incident/views.py
+++ incident/views.py
@@ -2,7 +2,7 @@ from django.contrib.postgres.search import SearchQuery, SearchVector
 from django.shortcuts import render
 from django.views.decorators.vary import vary_on_headers
 from wagtail.utils.pagination import paginate
-from wagtail.admin.forms import SearchForm
+from wagtail.admin.forms.search import SearchForm
 from wagtail.admin.utils import (
     user_has_any_page_permission,
     user_passes_test,
