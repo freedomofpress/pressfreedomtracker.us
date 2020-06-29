freedomofpress/pressfreedomtracker.us

commit 9382434a975a3dd1b8c3abd41678cfa8a1b019f1

filter ui: Handle native date objects as filter values
Version 2.0.0 of react-datepicker no longer generates Moment values as
output, instead using native JS date objects.  We must handle those
accordingly.

client/common/js/filtering/IncidentFiltering.js
@@ -320,7 +320,7 @@ class IncidentFiltering extends PureComponent {
 		let value
 		if (!event) {
 			value = null
-		} else if (isMoment(event)) {
+		} else if (isMoment(event) || event instanceof Date) {
 			value = event
 		} else if (event.target && event.target.hasOwnProperty('checked')) {
 			value = event.target.checked

commit 8957f31a8e9a0e93a90e426b56f345e974d7ff9a

filter ui: Convert moment values to JS date values before storing
Fixes a problem with code introduced in 9382434a975a3dd1b8c3abd41678cfa8a1b019f1

client/common/js/filtering/IncidentFiltering.js
@@ -73,8 +73,8 @@ class IncidentFiltering extends PureComponent {
 				if (filter.type === 'date') {
 					const lowerDate = `${filter.name}_lower`
 					const upperDate = `${filter.name}_upper`
-					if (params[lowerDate]) newAcc[lowerDate] = moment(params[lowerDate])
-					if (params[upperDate]) newAcc[upperDate] = moment(params[upperDate])
+					if (params[lowerDate]) newAcc[lowerDate] = moment(params[lowerDate]).toDate()
+					if (params[upperDate]) newAcc[upperDate] = moment(params[upperDate]).toDate()
 				} else if (filter.type === 'autocomplete' && params[filter.name]) {
 					if (filter.many) {
 						newAcc[filter.name] = params[filter.name]

client/common/js/filtering/Inputs.js
@@ -207,7 +207,7 @@ export class DateRangeInput extends PureComponent {
 		const moment = Moment(value, DATE_TYPES, true)
 
 		if (moment.isValid()) {
-			this.props.handleFilterChange(filter_key, moment)
+			this.props.handleFilterChange(filter_key, moment.toDate())
 		}
 	}
 