c6f6ad7e0d2a685f0c381fcffe2e82e13032971e
Cameron Higby-Naquin <me@chigby.org>
2021-04-13 14:42:34 -0400

    Enable API pagination

--- incident/api/views.py
+++ incident/api/views.py
@@ -1,14 +1,23 @@
 from rest_framework import viewsets
 from rest_framework.settings import api_settings
-from rest_framework_csv.renderers import CSVRenderer
+from rest_framework.pagination import CursorPagination
+from rest_framework_csv.renderers import PaginatedCSVRenderer
 
 from incident.api.serializers import IncidentSerializer, FlatIncidentSerializer
 from incident.utils.incident_filter import IncidentFilter
 
 
+class IncidentCursorPagination(CursorPagination):
+    page_size = 25
+    page_size_query_param = 'limit'
+    max_page_size = 200
+    ordering = '-unique_date'
+
+
 class IncidentViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = IncidentSerializer
-    renderer_classes = tuple(api_settings.DEFAULT_RENDERER_CLASSES) + (CSVRenderer,)
+    renderer_classes = tuple(api_settings.DEFAULT_RENDERER_CLASSES) + (PaginatedCSVRenderer,)
+    pagination_class = IncidentCursorPagination
 
     def get_serializer_class(self):
         if self.request.accepted_renderer.format == 'csv':
