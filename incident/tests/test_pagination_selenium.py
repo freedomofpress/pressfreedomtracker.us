3662d5faaacd8a79e02a773039d42bf7c247e061
Cameron Higby-Naquin <me@chigby.org>
2021-05-18 16:09:31 -0400

    Initial filtering and pagination tests

---
+++ incident/tests/test_pagination_selenium.py
@@ -0,0 +1,35 @@
+from wagtail.core.models import Page
+
+from common.tests.selenium import SeleniumTest
+from incident.tests.factories import IncidentIndexPageFactory, IncidentPageFactory
+
+
+class PaginationTestCase(SeleniumTest):
+    def setUp(self):
+        super().setUp()
+        root_page = Page.objects.get(slug='home')
+
+        self.index = IncidentIndexPageFactory(parent=root_page)
+
+        IncidentPageFactory.create_batch(30, parent=self.index)
+
+    def test_next_page_button(self):
+        more_css = '.js-incident-loading-next-link'
+        incident_css = 'article.incident'
+        incidents_per_page = 8
+
+        self.browser.get(self.live_server_url + self.index.url)
+
+        incidents = self.browser.find_elements_by_css_selector(incident_css)
+        expected_incidents = incidents_per_page
+        self.assertEqual(len(incidents), expected_incidents)
+
+        self.browser.find_element_by_css_selector(more_css).click()
+        incidents = self.browser.find_elements_by_css_selector(incident_css)
+        expected_incidents += incidents_per_page
+        self.assertEqual(len(incidents), expected_incidents)
+
+        self.browser.find_element_by_css_selector(more_css).click()
+        incidents = self.browser.find_elements_by_css_selector(incident_css)
+        expected_incidents += incidents_per_page
+        self.assertEqual(len(incidents), expected_incidents)
