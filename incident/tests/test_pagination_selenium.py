3c6cc9bef71e9a001569e5b474fdc92a561ab328
Cameron Higby-Naquin <me@chigby.org>
2021-05-25 17:37:12 -0400

    Wait for page to update before asserting

--- incident/tests/test_pagination_selenium.py
+++ incident/tests/test_pagination_selenium.py
@@ -1,7 +1,28 @@
 from wagtail.core.models import Page
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
 from common.tests.selenium import SeleniumTest
 from incident.tests.factories import IncidentIndexPageFactory, IncidentPageFactory
+
+
+class element_has_href(object):
+    """An expectation for checking that an element has a particular href value.
+
+    locator - used to find the element
+    returns the WebElement once it has the particular css class
+    """
+
+    def __init__(self, locator, css_class):
+        self.locator = locator
+        self.css_class = css_class
+
+    def __call__(self, driver):
+        element = driver.find_element(*self.locator)   # Finding the referenced element
+        if self.css_class in element.get_attribute("href"):
+            return element
+        else:
+            return False
 
 
 class PaginationTestCase(SeleniumTest):
@@ -25,11 +46,15 @@ class PaginationTestCase(SeleniumTest):
         self.assertEqual(len(incidents), expected_incidents)
 
         self.browser.find_element_by_css_selector(more_css).click()
+        wait = WebDriverWait(self.browser, 10)
+        wait.until(element_has_href((By.CSS_SELECTOR, more_css), "?page=3"))
         incidents = self.browser.find_elements_by_css_selector(incident_css)
         expected_incidents += incidents_per_page
         self.assertEqual(len(incidents), expected_incidents)
 
         self.browser.find_element_by_css_selector(more_css).click()
+        wait = WebDriverWait(self.browser, 10)
+        wait.until(element_has_href((By.CSS_SELECTOR, more_css), "?page=4"))
         incidents = self.browser.find_elements_by_css_selector(incident_css)
         expected_incidents += incidents_per_page
         self.assertEqual(len(incidents), expected_incidents)
