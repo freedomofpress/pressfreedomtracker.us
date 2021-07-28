a28802f431264b32d7645f05dea5f9964b50f9a8
Saptak S <saptak013@gmail.com>
2021-07-28 17:49:06 +0530

    Rearranges the preference for meta images in incidents

--- incident/models/incident_page.py
+++ incident/models/incident_page.py
@@ -800,8 +800,9 @@ class IncidentPage(MetadataPageMixin, Page):
     def get_meta_image(self):
         return (
             self.teaser_image or
-            self.get_main_category().default_image or
-            super(IncidentPage, self).get_meta_image()
+            self.search_image or
+            (self.get_main_category() and self.get_main_category().default_image) or
+            self._get_ssssettings().default_image
         )
 
     def get_meta_description(self):
