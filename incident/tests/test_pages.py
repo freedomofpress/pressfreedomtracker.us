d92821664d810353302140d38544109bfb26715d
Kevin <kevin.howbrook@torchbox.com>
2022-01-12 10:10:54 +0000

    Switch to build objects in setUp

--- incident/tests/test_pages.py
+++ incident/tests/test_pages.py
@@ -516,12 +516,12 @@ class GetIncidentUpdatesTest(TestCase):
 
 
 class IncidentPageStatisticsTagsTestCase(WagtailPageTests):
-    @classmethod
-    def setUpTestData(cls):
+    def setUp(self):
+        super().setUp()
         Page.objects.filter(slug='home').delete()
         root_page = Page.objects.get(title='Root')
-        cls.home_page = HomePageFactory.build(parent=None, slug='home')
-        root_page.add_child(instance=cls.home_page)
+        self.home_page = HomePageFactory.build(parent=None, slug='home')
+        root_page.add_child(instance=self.home_page)
 
         site, created = Site.objects.get_or_create(
             is_default_site=True,
@@ -529,18 +529,18 @@ class IncidentPageStatisticsTagsTestCase(WagtailPageTests):
                 'site_name': 'Test site',
                 'hostname': 'testserver',
                 'port': '1111',
-                'root_page': cls.home_page,
+                'root_page': self.home_page,
             }
         )
         if not created:
-            site.root_page = cls.home_page
+            site.root_page = self.home_page
             site.save()
 
-        cls.site = site
-        cls.category = CategoryPageFactory(parent=cls.home_page)
-        cls.author = PersonPageFactory(parent=cls.home_page)
-        cls.index_page = IncidentIndexPageFactory(
-            parent=cls.home_page,
+        self.site = site
+        self.category = CategoryPageFactory(parent=self.home_page)
+        self.author = PersonPageFactory(parent=self.home_page)
+        self.index_page = IncidentIndexPageFactory(
+            parent=self.home_page,
         )
 
     def test_can_preview_incident_page(self):
