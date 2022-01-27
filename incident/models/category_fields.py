c83f4ba4c5f685058e52e5ee0b569fbb02ecb256
Saptak S <saptak013@gmail.com>
2022-01-27 21:10:05 +0530

    Adds category field map constant

---
+++ incident/models/category_fields.py
@@ -0,0 +1,55 @@
+CATEGORY_FIELD_MAP = {
+    'arrest': [
+        ('arrest_status', 'Arrest Status'),
+        ('status_of_charges', 'Status of Charges'),
+        ('arresting_authority', 'Arresting Authority'),
+        ('current_charges', 'Current Charges'),
+        ('dropped_charges', 'Dropped Charges'),
+        ('detention_date', 'Detention Date'),
+        ('release_date', 'Release Date'),
+        ('unnecessary_use_of_force', 'Unnecessary use of force?'),
+    ],
+    'equipment-damage': [
+        ('equipment_broken', 'Equipment Broken'),
+    ],
+    'equipment-search': [
+        ('equipment_seized', 'Equipment Seized'),
+        ('status_of_seized_equipment', 'Status of Seized Equipment'),
+        ('is_search_warrant_obtained', 'Search Warrant Obtained'),
+        ('actor', 'Actor who seized equipment'),
+    ],
+    'border-stop': [
+        ('border_point', 'Border Point'),
+        ('target_nationality','Target Nationality'),
+        ('target_us_citizenship_status', 'US Citizenship Status of Target'),
+        ('denial_of_entry', 'Denied Entry?'),
+        ('stopped_previously', 'Stopped Previously?'),
+        ('did_authorities_ask_for_device_access', 'Asked for device access?'),
+        ('did_authorities_ask_for_social_media_user', 'Asked to display social media?'),
+        ('did_authorities_ask_for_social_media_pass', 'Asked for social media passwords?'),
+        ('did_authorities_ask_about_work', 'Asked intrusive questions about work?'),
+    ],
+    'physical-attack': [
+        ('assailant', 'Assailant'),
+        ('was_journalist_targeted', 'Was the journalist targeted?'),
+    ],
+    'leak-case': [
+        ('workers_whose_communications_were_obtained', 'Targets whose Communications Were Obtained'),
+        ('charged_under_espionage_act', 'Charged under Espionage Act'),
+    ],
+    'subpoena': [
+        ('subpoena_type', 'Subpoena Type'),
+        ('subpoena_statuses', 'Subpoena Status'),
+        ('held_in_contempt', 'Journalist held in contempt?'),
+        ('detention_status', 'Detention Status'),
+        ('third_party_in_possession_of_communications', 'Third Party in Possession of Communications'),
+        ('third_party_business', 'Third Party Business'),
+        ('legal_order_type', 'Legal Order Type'),
+    ],
+    'prior-restraint': [
+        ('status_of_prior_restraint', 'Status of Prior Restraint')
+    ],
+    'denial-of-access': [
+        ('politicians_or_public_figures_involved', 'Politicians or Public Figures Involved'),
+    ],
+}
