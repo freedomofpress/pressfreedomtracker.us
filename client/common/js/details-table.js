f6dff92f317fc0e9ee271b52244f392bb89bd36d
Cameron Higby-Naquin <me@chigby.org>
2022-02-22 18:35:28 -0500

    Align database card columns with page grid
    There's a lot going on in this commit, as I restructured a lot of
    things, which broke some other things (including the "Show more/less"
    button, which may have been partially broken this whole time) so I
    took the time to go through and clean up a lot of stuff in addition to
    aligning the columns.
    
    Note: At "narrow desktop" widths, we are running into longish columns
    overflowing their boundaries.  Probably should fix this.

--- client/common/js/details-table.js
+++ client/common/js/details-table.js
@@ -1,20 +1,13 @@
-if (document.querySelector('#details-toggle-btn')) {
-	const detailsBtn = document.querySelector('#details-toggle-btn')
-	const detailsDiv = document.querySelector('#incident-details')
-
-	detailsDiv.dataset.visible = 'false'
-	detailsBtn.setAttribute('aria-expanded', 'false')
-	detailsBtn.classList.add('has-js')
-
-	detailsBtn.addEventListener('click', () => {
-		if (detailsDiv.dataset.visible === 'false') {
-			detailsDiv.dataset.visible = 'true'
-			detailsBtn.setAttribute('aria-expanded', 'true')
-			detailsBtn.innerText = 'Show Less'
-		} else if (detailsDiv.dataset.visible === 'true') {
-			detailsDiv.dataset.visible = 'false'
-			detailsBtn.setAttribute('aria-expanded', 'false')
-			detailsBtn.innerText = 'Show More'
-		}
+document.querySelectorAll('.incident-database-card__details-toggle').forEach(button => {
+	button.addEventListener('click', (e) => {
+		let isVisible = Boolean(e.target.dataset.visible === 'true')
+		let newVisible = !isVisible
+		let newText = newVisible ? "Show Less" : "Show More"
+		e.target.innerText = newText
+		e.target.dataset.visible = newVisible.toString()
+		e.target.setAttribute('aria-expanded', newVisible.toString())
+		e.target.getAttribute('aria-controls').split(' ').forEach(id => {
+			document.getElementById(id).dataset.visible = newVisible.toString()
+		})
 	})
-}
+})
