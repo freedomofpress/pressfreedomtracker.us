af7cd19b4971d615ad347ad9fb41827ac60348c9
Saptak S <saptak013@gmail.com>
2022-02-22 19:22:48 +0530

    Adds JS fallback for the details table logic

--- client/common/js/details-table.js
+++ client/common/js/details-table.js
@@ -2,6 +2,11 @@ if (document.querySelector('#details-toggle-btn')) {
 	const detailsBtn = document.querySelector('#details-toggle-btn')
 	const detailsDiv = document.querySelector('#incident-details')
 
+	detailsDiv.dataset.visible = 'false'
+	detailsBtn.setAttribute('aria-expanded', 'false')
+	detailsBtn.classList.add('has-js')
+
+
 	detailsBtn.addEventListener('click', () => {
 		if (detailsDiv.dataset.visible === 'false') {
 			detailsDiv.dataset.visible = 'true'
