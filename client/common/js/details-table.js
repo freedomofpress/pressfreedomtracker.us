433509f7785b066fa86b7b3cc078d2968675ec9b
Cameron Higby-Naquin <me@chigby.org>
2022-02-23 10:20:56 -0500

    Fix eslint errors

--- client/common/js/details-table.js
+++ client/common/js/details-table.js
@@ -1,12 +1,12 @@
-document.querySelectorAll('.incident-database-card__details-toggle').forEach(button => {
+document.querySelectorAll('.incident-database-card__details-toggle').forEach((button) => {
 	button.addEventListener('click', (e) => {
-		let isVisible = Boolean(e.target.dataset.visible === 'true')
-		let newVisible = !isVisible
-		let newText = newVisible ? "Show Less" : "Show More"
+		const isVisible = Boolean(e.target.dataset.visible === 'true')
+		const newVisible = !isVisible
+		const newText = newVisible ? 'Show Less' : 'Show More'
 		e.target.innerText = newText
 		e.target.dataset.visible = newVisible.toString()
 		e.target.setAttribute('aria-expanded', newVisible.toString())
-		e.target.getAttribute('aria-controls').split(' ').forEach(id => {
+		e.target.getAttribute('aria-controls').split(' ').forEach((id) => {
 			document.getElementById(id).dataset.visible = newVisible.toString()
 		})
 	})
