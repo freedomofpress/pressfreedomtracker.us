c007a5c41a6a6ccf8744c5dcc894dd02bf682d69
Cameron Higby-Naquin <me@chigby.org>
2022-03-01 10:10:23 -0500

    Fix JS on incident details page (and other pages)
    I think I accidentally removed the `details-toggle-button` code while
    trying to fix a merge conflict.

--- client/common/js/details-table.js
+++ client/common/js/details-table.js
@@ -1,3 +1,24 @@
+if (document.querySelector('#details-toggle-btn')) {
+	const detailsBtn = document.querySelector('#details-toggle-btn')
+	const detailsDiv = document.querySelector('#incident-details')
+
+	detailsDiv.dataset.visible = 'false'
+	detailsBtn.setAttribute('aria-expanded', 'false')
+	detailsBtn.classList.add('has-js')
+
+	detailsBtn.addEventListener('click', () => {
+		if (detailsDiv.dataset.visible === 'false') {
+			detailsDiv.dataset.visible = 'true'
+			detailsBtn.setAttribute('aria-expanded', 'true')
+			detailsBtn.innerText = 'Show Less'
+		} else if (detailsDiv.dataset.visible === 'true') {
+			detailsDiv.dataset.visible = 'false'
+			detailsBtn.setAttribute('aria-expanded', 'false')
+			detailsBtn.innerText = 'Show More'
+		}
+	})
+}
+
 document.querySelectorAll('.incident-database-card__details-toggle').forEach((button) => {
 	button.addEventListener('click', (e) => {
 		const isVisible = Boolean(e.target.dataset.visible === 'true')
