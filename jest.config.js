75a18173642cf5988012e01582e68eec46d53ea2
Harris Lapiroff <harris@freedom.press>
2022-03-09 16:15:52 -0500

    Add a bunch of modules to be transformed for jest purposes
    I'm not entirely sure why these modules aren't already transpiled
    (more research needed) but exempting them from `transformIgnorePattens`
    allows jest tests to run

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,8 @@ module.exports = {
     '^~/(.*)$': '<rootDir>/client/common/js/$1',
     '^WagtailAutocomplete/(.*)$': '<rootDir>/client/autocomplete/js/components/$1',
   },
+	transformIgnorePatterns: [
+		"<rootDir>/node_modules/(?!d3|internmap|delaunator|robust-predicates)"
+	],
   setupFiles: ["<rootDir>/client/common/js/setupTests.js"],
 }
