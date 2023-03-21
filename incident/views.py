cce81c502a2ca9d81d24dbfa5099eba3748ac729
Cameron Higby-Naquin <me@chigby.org>
2023-03-21 11:03:54 -0400

    formatting: Reindent and sort imports

--- incident/views.py
+++ incident/views.py
@@ -3,15 +3,31 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.decorators.vary import vary_on_headers
 from django.views.generic.edit import FormView
-from wagtail.admin.forms.search import SearchForm
 from wagtail.admin.auth import (
     user_has_any_page_permission,
     user_passes_test,
 )
+from wagtail.admin.forms.search import SearchForm
 
 from common.views import MergeView
-from incident.forms import ChargeMergeForm, VenueMergeForm, NationalityMergeForm, PoliticianOrPublicMergeForm, JournalistMergeForm, InstitutionMergeForm, GovernmentWorkerMergeForm, LawEnforcementOrganizationForm
-from incident.models import IncidentPage, Journalist, TargetedJournalist, Institution, Charge, IncidentCharge
+from incident.forms import (
+    ChargeMergeForm,
+    GovernmentWorkerMergeForm,
+    InstitutionMergeForm,
+    JournalistMergeForm,
+    LawEnforcementOrganizationForm,
+    NationalityMergeForm,
+    PoliticianOrPublicMergeForm,
+    VenueMergeForm,
+)
+from incident.models import (
+    Charge,
+    IncidentCharge,
+    IncidentPage,
+    Institution,
+    Journalist,
+    TargetedJournalist,
+)
 
 
 @vary_on_headers('X-Requested-With')
