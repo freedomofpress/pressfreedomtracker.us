775a92431c124b5325df4abb6d624550a096bf89
Cameron Higby-Naquin <me@chigby.org>
2023-05-25 12:48:15 -0400

    Rework category chooser for incident sets as checklist

--- common/models/helpers.py
+++ common/models/helpers.py
@@ -3,3 +3,13 @@ def get_tags():
     from common.models import CommonTag  # Avoids circular import
 
     return [(tag.title, tag.title) for tag in CommonTag.objects.all()]
+
+
+def get_categories():
+    """Get category names in a form appropriate for model choices"""
+    from common.models import CategoryPage  # Avoids circular import
+
+    return [
+        (page.title, page.title) for page
+        in CategoryPage.objects.order_by('title').live()
+    ]
