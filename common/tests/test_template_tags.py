b9d8360d6c1dd10b5dd41a19ac4a96ee5f15a427
Saptak S <saptak013@gmail.com>
2023-05-25 16:27:17 +0530

    Adds tests for richtext aside templatetag

--- common/tests/test_template_tags.py
+++ common/tests/test_template_tags.py
@@ -2,7 +2,7 @@ from unittest import mock
 
 from django.test import TestCase
 
-from common.templatetags.common_tags import lookup, add_as_string
+from common.templatetags.common_tags import lookup, add_as_string, richtext_aside
 
 
 class TestTemplateTags(TestCase):
@@ -41,3 +41,13 @@ class TestTemplateTags(TestCase):
         bad_object.__radd__.return_value = 'I was added to something'
 
         self.assertEqual(add_as_string(part_string, bad_object), 'I was added to something')
+
+    def test_richtext_aside(self):
+        paragraph_aside = '<p data-block-key="89kgh">This is paragraph with <b>bold text</b></p>'
+        paragraph_aside_with_span = '<p data-block-key="89kgh"><span>This is paragraph with <b>bold text</b></span></p>'
+
+        heading_aside = '<h3 data-block-key="89agh">This is a heading</h3>'
+        heading_aside_with_span = '<h3 data-block-key="89agh"><span>This is a heading</span></h3>'
+
+        self.assertEqual(richtext_aside(paragraph_aside), paragraph_aside_with_span)
+        self.assertEqual(richtext_aside(heading_aside), heading_aside_with_span)
