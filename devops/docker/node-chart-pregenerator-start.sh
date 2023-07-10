d38b0f938d72b31a6be13bac5d2bbfedb1e0c0b0
Daniel Kao <dkao@diplateevo.com>
2023-07-10 13:13:19 -0700

    clean up script

--- devops/docker/node-chart-pregenerator-start.sh
+++ devops/docker/node-chart-pregenerator-start.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
-#
-# Start installing node dependencies and leave downstream containers
-# a file to poll when we are ready to go
 
 set -x
 
 cd chart_pregenerator
-pwd
-pwd
-pwd
-pwd
 
 npm install && \
     touch .node_complete && \
