ee024784dab18e868cb59e4c558431f5099d161c
Cameron Higby-Naquin <me@chigby.org>
2023-08-07 16:50:33 -0400

    Add ChartSnapshot model
    Here, I'm adding a model to store/cache the output of the
    pregeneration service.  It's called `ChartSnapshot` and it records
    calls made to the service, what the service returned, and what time
    that happened.
    
    I've included very basic logic for knowing when the cache is too
    old (if it was last generated over 24 hours ago).  I've also put a
    constraint on the model that it must have at least one value for
    `chart_svg` or `chart_image` to be able to be saved, meaning if the
    service didn't return anything, we don't have a valid snapshot.

--- common/models/__init__.py
+++ common/models/__init__.py
@@ -1,3 +1,4 @@
 from common.models.customimage import *  # noqa: F403, F401
 from common.models.pages import *  # noqa: F403, F401
 from common.models.settings import *  # noqa: F403, F401
+from common.models.charts import *  # noqa: F403, F401
