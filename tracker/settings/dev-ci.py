67544ee50f9cb05cbcb880bffd0a3d1a4be2d69c
Cameron Higby-Naquin <me@chigby.org>
2023-08-23 17:39:27 -0400

    Configure Django test suite JUnit XML output folder
    Making it the same folder as the jest tests so we can have circle ci
    grab them both at once.

---
+++ tracker/settings/dev-ci.py
@@ -0,0 +1,7 @@
+from .dev import *  # noqa: F403, F401
+
+TEST_RUNNER = 'common.test_runner.SeededXMLRunner'
+TEST_OUTPUT_DIR = "./test-results/django"
+TEST_OUTPUT_FILE_NAME = "results.xml"
+TEST_OUTPUT_DESCRIPTIONS = True
+TEST_OUTPUT_VERBOSE = 2
