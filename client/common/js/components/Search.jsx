81f2b143d3325bfc0c3f8fdd40d9e4576eae6144
Daniel Kao <dkao@diplateevo.com>
2023-10-20 14:21:05 -0700

    Smart Search

--- client/common/js/components/Search.jsx
+++ client/common/js/components/Search.jsx
@@ -1,45 +1,89 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react'
 import classNames from 'classnames'
-// eslint-disable-next-line import/named
 import PropTypes from 'prop-types'
 import { chooseMostFrequentTags } from '../../../charts/js/components/HomepageSelection'
 
-const numberOfTags = 5
+const numberOfTags = 4
 
 export default function Search({ data = [] }) {
 	const [searchActive, setSearchActive] = useState(false)
+	const [selectedTag, setSelectedTag] = useState()
+	const [searchText, setSearchText] = useState('')
 
-	const selectedTags = chooseMostFrequentTags(data, numberOfTags)
-	console.log(selectedTags)
+	const frequentTags = chooseMostFrequentTags(data, numberOfTags)
+
+	const updateSelectedTag = (tag) => {
+		setSearchText('')
+		setSelectedTag(tag)
+	}
+
+	const updateSearchText = (text) => {
+		if (selectedTag) return
+		setSelectedTag(null)
+		setSearchText(text)
+	}
+
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		if (searchText) {
+			window.location.href = `/all-incidents/?search=${searchText}`
+		} else if (selectedTag) {
+			window.location.href = `/all-incidents/?tags=${selectedTag}`
+		}
+	}
 
 	// The base markup is the same as incidents/templates/incident/_search_bar.html
 	return (
-		<form className="search-form">
+		<form className="search-form" onSubmit={handleSubmit}>
 			<label htmlFor="primary-search-bar" className="sr-only">
 				Search incidents by text
 			</label>
 			<input
 				id="primary-search-bar"
-				placeholder="Search incidents by text"
+				placeholder={!selectedTag ? 'Search incidents' : ''}
 				spellCheck="false"
 				autoComplete="off"
 				type="search"
 				className={classNames('text-field--search-bar', searchActive && 'smart-search-active')}
 				name="search"
+				value={searchText}
 				onFocus={() => setSearchActive(true)}
-				onBlur={() => setSearchActive(false)}
+				onBlur={() => setTimeout(() => setSearchActive(false), 200)}
+				onChange={(e) => updateSearchText(e.target.value)}
 			/>
 			<button type="submit" className="btn btn-ghost search-button" value="Search">
 				Search
 			</button>
 
-			{searchActive && (
+			{searchActive && !selectedTag && !searchText && !!frequentTags.length && (
 				<div className="search-dropdown">
 					<div className="search-dropdown--header">Trending Topics</div>
-					<div className="search-dropdown--category">
-						Test Category
-					</div>
+					{frequentTags.map((tag) => (
+						<button
+							type="button"
+							key={tag}
+							className="search-dropdown--category"
+							onClick={() => updateSelectedTag(tag)}
+						>
+							<span className="search-dropdown--category--tag-hash">#</span>
+							{tag}
+						</button>
+					))}
+				</div>
+			)}
+
+			{selectedTag && (
+				<div className="search-tag-pill">
+					<span className="search-tag-pill--tag-hash">#</span>
+					{selectedTag}
+					<button
+						type="button"
+						className="search-tag-pill--close"
+						onClick={() => updateSelectedTag(null)}
+					>
+						<i className="search-tag-pill--close--icon" />
+					</button>
 				</div>
 			)}
 		</form>
