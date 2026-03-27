6b0b3592008a4c28b65d6fbb68b92a52ae42e804
Saptak S <saptak013@gmail.com>
2026-03-27 20:35:51 +0530

    Adds middleware to add Onion-Location header with full path

--- tracker/settings/production.py
+++ tracker/settings/production.py
@@ -21,7 +21,8 @@ if os.environ.get('DJANGO_HOST'):
     ALLOWED_HOSTS += [os.environ['DJANGO_HOST']]  # noqa: F405
 
 
-MIDDLEWARE.insert(1, 'common.middleware.RequestLogMiddleware')  # noqa: F405
+MIDDLEWARE.insert(1, 'common.middleware.request_logger.RequestLogMiddleware')  # noqa: F405
+MIDDLEWARE.append('common.middleware.onion_location.OnionLocationHeaderMiddleware')  # noqa: F405
 
 
 structlog.configure(
