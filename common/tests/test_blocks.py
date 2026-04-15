b3afc56d4b3b08bb84e689b73482cacaf85232c2
Cameron Higby-Naquin <me@chigby.org>
2026-04-15 16:30:57 -0400

    Fix flake8 complaint about `type` comparisons
    Also fix tests that were failing due to not using the wagtail types.

--- common/tests/test_blocks.py
+++ common/tests/test_blocks.py
@@ -1,24 +1,23 @@
 from django.test import TestCase
 
 from common.blocks import AlignedCaptionedImageBlock
-from common.tests.factories import AlignedCaptionedImageBlockFactory
 
 
 class AlignedCaptionedImageBlockGetContextTest(TestCase):
     def test_get_context_with_content_warning_unwraps_first_item(self):
+        warning_text = 'This image may be disturbing.'
         block = AlignedCaptionedImageBlock()
-        value = AlignedCaptionedImageBlockFactory()
-        value['content_warning'] = ['This image may be disturbing.']
-
+        value = block.to_python({
+            'content_warning': [warning_text],
+        })
         block.get_context(value)
-
-        self.assertEqual(value['content_warning'], 'This image may be disturbing.')
+        self.assertEqual(value['content_warning'], warning_text)
 
     def test_get_context_without_content_warning_leaves_value_unchanged(self):
         block = AlignedCaptionedImageBlock()
-        value = AlignedCaptionedImageBlockFactory()
-        value['content_warning'] = []
+        value = block.to_python({
+            'content_warning': [],
+        })
 
         block.get_context(value)
-
-        self.assertEqual(value['content_warning'], [])
+        self.assertEqual(list(value['content_warning']), [])
