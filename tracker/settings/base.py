1a2897d1751e13a24cf137bfcde8caf1baba9f4f
Paul Schreiber <paul@freedom.press>
2026-07-02 16:34:41 -0400

    fix: add WHITENOISE_ROOT to settings

--- tracker/settings/base.py
+++ tracker/settings/base.py
@@ -117,6 +117,7 @@ if os.environ.get("DJANGO_WHITENOISE"):
     STORAGES["staticfiles"] = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     }
+    WHITENOISE_ROOT = os.path.join(BASE_DIR, "common", "static")
 
 MIDDLEWARE.extend(
     [
