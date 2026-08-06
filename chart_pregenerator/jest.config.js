59134fab9d729cc953303f111009e297b504fc56
Ryan Whitney <ryan.w@freedom.press>
2026-08-06 14:36:55 -0700

    replace babel with swc in chart_pregenerator

--- chart_pregenerator/jest.config.js
+++ chart_pregenerator/jest.config.js
@@ -1,7 +1,13 @@
+// The Dockerfiles copy swc.config.js one level above the working directory, so
+// this relative path resolves both in the repo and in the container.
+const swcConfig = require('../swc.config')
+
 module.exports = {
 	transform: {
-		'^.+\\.jsx?$': 'babel-jest',
+		'^.+\\.jsx?$': ['@swc/jest', swcConfig],
 	},
+	// d3 and friends ship ESM only, so they need transpiling despite living in
+	// node_modules.
 	transformIgnorePatterns: [
 		'<rootDir>/node_modules/(?!d3|tracker|internmap|delaunator|robust-predicates|react-animated-dataset)',
 	],
