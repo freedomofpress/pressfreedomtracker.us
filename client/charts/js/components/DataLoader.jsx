6df7c8c4452c0d0a70e78f32e1e53c99bd6d2608
Ryan Whitney <ryan.w@freedom.press>
2026-08-06 14:36:04 -0700

    Remove unused packages

--- client/charts/js/components/DataLoader.jsx
+++ client/charts/js/components/DataLoader.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import * as d3 from 'd3'
-import 'regenerator-runtime/runtime'
 
 function Loader() {
 	return <div>Loading...</div>
