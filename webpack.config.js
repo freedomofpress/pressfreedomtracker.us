af54cbeedb5a1af4b1eacfc0fd8867290138b960
Paul Schreiber <paul@freedom.press>
2026-09-09 12:29:35 -0400

    fix: rename sassData to scssData

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ process.env.BABEL_ENV = TARGET;
 var target = __dirname + "/build/static/bundles";
 
 var STATIC_URL = process.env.STATIC_URL || "/common/static/";
-var sassData = '$static-url: "' + STATIC_URL + '";';
+var scssData = '$static-url: "' + STATIC_URL + '";';
 console.log("Using STATIC_URL", STATIC_URL);
 
 var common = {
@@ -81,7 +81,7 @@ var common = {
 							sassOptions: {
 								includePaths: [path.resolve(__dirname, "node_modules/")],
 							},
-							additionalData: sassData,
+							additionalData: scssData,
 						},
 					},
 				],
